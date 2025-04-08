afc38ebd670765e79d904ac2369b749c6ce7f1fd
Alex Iribarren <alex.iribarren@gmail.com>
2025-04-08 10:48:06 +0200

    Add system_files

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,13 +5,17 @@ set -ouex pipefail
 CONTEXT_PATH="$(realpath "$(dirname "$0")/..")" # should return /run/context
 BUILD_SCRIPTS_PATH="$(realpath "$(dirname "$0")")"
 
+printf "::group:: === Copying files ===\n"
+cp -avf "${CONTEXT_PATH}/system_files/." /
+printf "::endgroup::\n"
+
 for script in $(find ${BUILD_SCRIPTS_PATH} -maxdepth 1 -iname "*-*.sh" -type f -print0 | sort --sort=human-numeric); do
-    printf "::group:: ===$script-%s===\n" "$(basename "$script")"
+    printf "::group:: === $(basename "$script") ===\n"
 		"$(realpath $script)"
     printf "::endgroup::\n"
 done
 
-printf "::group:: ===Image Cleanup===\n"
+printf "::group:: === Image Cleanup ===\n"
 # Ensure these get run at the _end_ of the build no matter what
 "${BUILD_SCRIPTS_PATH}/cleanup.sh"
 printf "::endgroup::\n"
